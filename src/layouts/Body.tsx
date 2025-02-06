05f866e0913a3983a8dce519d2c2bec826dec806
Irana06 <kirainova11@gmail.com>
2025-02-06 08:00:20 +0700

    feat: Homepage

---
+++ src/layouts/Body.tsx
@@ -0,0 +1,7 @@
+export const Body: React.FC = () => {
+  return (
+    <div className="flex-1 bg-black text-white flex items-center justify-center pb-6">
+      <h1>Isi Konten</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Navbar } from './layouts/Navbar.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+import { Navbar } from "./layouts/Navbar.tsx";
+import { Footer } from "./layouts/Footer.tsx";
+import { Body } from "./layouts/Body.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Navbar />
-  </StrictMode>,
-)
+    <div className="flex flex-col min-h-screen gap-y-4">
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  </StrictMode>
+);
